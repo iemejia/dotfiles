63a8a4d497393fa0774c254aab1ed6cf3537fdf3
Ismael Mejia <iemejia@gmail.com>
2012-12-17 14:58:19 +0100

    Added transparency options

--- init.el
+++ init.el
@@ -23,6 +23,50 @@
                             (height . 50)
                             ))
 
+;; ;;
+;; ;; Transparency (taken from:
+;; ;; https://github.com/jimeh/.emacs.d/blob/master/helpers.el
+;; ;;
+
+;; (defun transparency-set-initial-value ()
+;;   "Set initial value of alpha parameter for the current frame"
+;;   (interactive)
+;;   (if (equal (frame-parameter nil 'alpha) nil)
+;;       (set-frame-parameter nil 'alpha 100)))
+
+;; (defun transparency-set-value (numb)
+;;   "Set level of transparency for the current frame"
+;;   (interactive "nEnter transparency level in range 0-100: ")
+;;   (if (> numb 100)
+;;       (message "Error! The maximum value for transparency is 100!")
+;;     (if (< numb 0)
+;;         (message "Error! The minimum value for transparency is 0!")
+;;       (set-frame-parameter nil 'alpha numb))))
+
+;; (defun transparency-increase ()
+;;   "Increase level of transparency for the current frame"
+;;   (interactive)
+;;   (transparency-set-initial-value)
+;;   (if (> (frame-parameter nil 'alpha) 0)
+;;       (set-frame-parameter nil 'alpha (+ (frame-parameter nil 'alpha) -1))
+;;     (message "This is a minimum value of transparency!")))
+
+;; (defun transparency-decrease ()
+;;   "Decrease level of transparency for the current frame"
+;;   (interactive)
+;;   (transparency-set-initial-value)
+;;   (if (< (frame-parameter nil 'alpha) 100)
+;;       (set-frame-parameter nil 'alpha (+ (frame-parameter nil 'alpha) +1))
+;;     (message "This is a minimum value of transparency!")))
+
+;; ;; Transparency (via helpers.el)
+;; (setq transparency-level 85)
+;; (transparency-set-value transparency-level)
+;; (add-hook 'after-make-frame-functions
+;;           (lambda (selected-frame)
+;;             (set-frame-parameter selected-frame 'alpha transparency-level)))
+
+
 ;(require 'zoom-frm)
 
 ;; use hippie-expand instead of dabbrev
@@ -79,6 +123,10 @@
   (global-set-key (kbd "<M-down>") 'forward-paragraph)
 
   (global-set-key (kbd "s-o") 'find-file)
+
+  (global-set-key (kbd "s-}") 'next-buffer)
+  (global-set-key (kbd "s-{") 'previous-buffer)
+
 )
 
 ;;  (global-set-key (kbd "s-<down>") 'forward-paragraph))
