e9e62d3c687dac9283593e31e640cdc32a3201dc
Ismael Mejia <iemejia@gmail.com>
2013-09-05 15:36:03 +0200

    Added script that auto installs everything in a new machine

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ln -s ~/repositories/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/repositories/dotfiles/.emacs.d ~/.emacs.d
+ln -s ~/repositories/dotfiles/.gdbinit ~/.gdbinit
+ln -s ~/repositories/dotfiles/.git-completion.bash ~/.git-completion.bash
+ln -s ~/repositories/dotfiles/.git-prompt.sh ~/.git-prompt.sh
+ln -s ~/repositories/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/repositories/dotfiles/.inputrc ~/.inputrc
+ln -s ~/repositories/dotfiles/.irssi ~/.irssi
+ln -s ~/repositories/dotfiles/.latexmkrc ~/.latexmkrc
+ln -s ~/repositories/dotfiles/.profile ~/.profile
+ln -s ~/repositories/dotfiles/.screenrc ~/.screenrc
+ln -s ~/repositories/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/repositories/dotfiles/.vim ~/.vim
+ln -s ~/repositories/dotfiles/.vimrc ~/.vimrc
+
