65e92570f6bdb698e33066ec067797e4b66a0035
Ismaël Mejía <iemejia@gmail.com>
2014-08-24 22:10:48 +0200

    Changed fonts (thinking about new static-resources-project).

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ ln -s ~/repositories/dotfiles/.zshrc ~/.zshrc
 ln -s ~/repositories/dotfiles/.zshenv ~/.zshenv
 ln -s ~/repositories/dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/repositories/dotfiles/.zsh-base16 ~/.zsh-base16
-ln -s ~/repositories/dotfiles/.fonts ~/.fonts
+cp ~/repositories/dotfiles/.fonts/* ~/.fonts/
 ln -s ~/repositories/dotfiles/.fonts.conf.d  ~/.fonts.conf.d
 ln -s ~/repositories/dotfiles/.gtkrc-2.0 ~/.gtkrc-2.0
 ln -s ~/repositories/dotfiles/s3.exclude ~/s3.exclude
