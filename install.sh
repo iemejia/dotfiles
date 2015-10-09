2e0386f55886b59f55b1e64e00df205140f3c579
Ismaël Mejía <iemejia@gmail.com>
2015-10-09 14:33:19 +0200

    Add .hiverc configuration

--- install.sh
+++ install.sh
@@ -31,5 +31,6 @@ ln -sfv ~/repositories/dotfiles/s3.exclude ~/s3.exclude
 ln -sfv ~/repositories/dotfiles/.mplayer ~/.mplayer
 ln -sfv ~/Dropbox/Documents/research-notes ~/research-notes
 ln -s ~/repositories/dotfiles/.git_commit_msg.txt ~/.git_commit_msg.txt
+ln -sfv ~/repositories/dotfiles/.hiverc ~/.hiverc
 
 fc-cache -vf ~/.fonts
