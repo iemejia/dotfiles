ea249c737bfc3dd4df958d611b36cff0ec0df564
Ismaël Mejía <iemejia@gmail.com>
2016-08-12 09:28:01 +0200

    Add support for docker updates + fixes in git pull updates

--- update.sh
+++ update.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
+set -x
 sudo apt-get update
 sudo apt-get -y dist-upgrade
 sudo apt-get autoclean
 sudo apt-get autoremove
 
 docker images | grep -v talend | awk '{print $1":"$2}' | xargs -L1 docker pull
+docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 
 git -C ~/.oh-my-zsh pull
 git -C ~/.bash_it pull
 
-for d in ~/.vim/bundle/ ; do
-  echo "$d"; cd "$d"; git fetch -p --all; git pull upstream; cd ..
+for d in ~/.vim/bundle/* ; do
+  echo "$d"; cd "$d"; git fetch -p --all; git pull; cd
 done
 
-for d in ~/upstream/ ; do
-  echo "$d"; cd "$d"; git fetch -p --all; git pull upstream; cd ..
+for d in ~/upstream/* ; do
+  echo "$d"; cd "$d"; git fetch -p --all; git pull; cd
 done
 
