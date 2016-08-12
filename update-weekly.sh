12d7e1cd638210fcfdabe89ee0854f954dd15bc5
Ismaël Mejía <iemejia@gmail.com>
2016-08-12 09:33:42 +0200

    Break update script into daily and weekly updates

---
+++ update-weekly.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+. update.sh
+
+docker images | grep -v talend | awk '{print $1":"$2}' | xargs -L1 docker pull
+docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
+git -C ~/.oh-my-zsh pull
+git -C ~/.bash_it pull
+
+for d in ~/.vim/bundle/* ; do
+  echo "$d"; cd "$d"; git fetch -p --all; git pull; cd
+done
+
+for d in ~/upstream/* ; do
+  echo "$d"; cd "$d"; git fetch -p --all; git pull; cd
+done
+
--- update.sh
+++ update.sh
@@ -5,18 +5,3 @@ sudo apt-get -y dist-upgrade
 sudo apt-get autoclean
 sudo apt-get autoremove
 
-docker images | grep -v talend | awk '{print $1":"$2}' | xargs -L1 docker pull
-docker rm $(docker ps -qa --no-trunc --filter "status=exited")
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
-
-git -C ~/.oh-my-zsh pull
-git -C ~/.bash_it pull
-
-for d in ~/.vim/bundle/* ; do
-  echo "$d"; cd "$d"; git fetch -p --all; git pull; cd
-done
-
-for d in ~/upstream/* ; do
-  echo "$d"; cd "$d"; git fetch -p --all; git pull; cd
-done
-
