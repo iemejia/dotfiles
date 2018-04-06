c660137343968e967c8feaf6b8ecca13e57345b4
Ismaël Mejía <iemejia@gmail.com>
2018-04-06 14:12:28 +0200

    Move update script from apt-get to apt

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
-sudo apt-get update
-sudo apt-get -y --allow-downgrades dist-upgrade
-sudo apt-get -y autoclean
-sudo apt-get -y autoremove
+sudo apt update
+sudo apt full-upgrade
+sudo apt autoclean
+sudo apt autoremove
 
