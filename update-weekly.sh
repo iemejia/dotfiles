b1506561e508fbf3ecb761ecf1ef2dd9abb6f7d0
Ismaël Mejía <iemejia@gmail.com>
2018-08-18 09:18:43 +0200

    Update weekly script update

--- update-weekly.sh
+++ update-weekly.sh
@@ -13,14 +13,16 @@ for d in ~/upstream/* ; do
   echo "$d"; cd "$d"; git fetch -p --all; git pull; cd
 done
 
-workon personal
+. ~/.virtualenvs/python2/personal/bin/activate
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 deactivate
 
-workon scientific
+. ~/.virtualenvs/python2/work/bin/activate
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 deactivate
 
-npm outdated -g --depth=0
+#npm outdated -g --depth=0
 npm update -g
+
+gcloud components update -q
 
