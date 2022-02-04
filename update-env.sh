7a4aa5836e22f75e82238287637188274adf2631
Ismaël Mejía <iemejia@gmail.com>
2022-02-04 09:03:25 +0100

    Update weekly updates scripts

--- update-env.sh
+++ update-env.sh
@@ -8,15 +8,15 @@ for d in ~/.vim/bundle/* ; do
   echo "$d"; cd "$d"; git fetch -p --all; git pull; cd
 done
 
-. ~/.virtualenvs/python3/personal/bin/activate
-pip install --upgrade pip setuptools wheel
-pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
-deactivate
+for d in ~/.virtualenvs/* ; do
+  source "$d"/bin/activate
+  pip install --upgrade pip setuptools wheel
+  pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+  #pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+  deactivate
+done
 
-. ~/.virtualenvs/python3/work/bin/activate
-pip install --upgrade pip setuptools wheel
-pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
-deactivate
+
 
 #npm outdated -g --depth=0
 npm update -g
