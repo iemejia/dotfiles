86d61821205e2b43cd964b505f958712ee780f28
Ismaël Mejía <iemejia@gmail.com>
2022-04-15 08:40:41 +0200

    [shell] create separated update scripts installer

---
+++ install-dev.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.local/bin
+
+ln -sfv ~/repositories/dotfiles/update-docker.sh ~/.local/bin/update-docker.sh
+ln -sfv ~/repositories/dotfiles/update-env.sh ~/.local/bin/update-env.sh
+ln -sfv ~/repositories/dotfiles/update-linux.sh ~/.local/bin/update-linux.sh
+ln -sfv ~/repositories/dotfiles/update-mac.sh ~/.local/bin/update-mac.sh
+ln -sfv ~/repositories/dotfiles/update-repositories.sh ~/.local/bin/update-repositories.sh
+ln -sfv ~/repositories/dotfiles/update-weekly.sh ~/.local/bin/update-weekly.sh
+
+
