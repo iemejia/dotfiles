65ed4d9d065291f61f84b8c39d2784f140c0d7f8
Ismaël Mejía <iemejia@gmail.com>
2023-01-06 09:44:00 +0100

    [git] Add missing repositories paths

--- update-repositories.sh
+++ update-repositories.sh
@@ -24,3 +24,18 @@ for d in ~/repositories/*; do
 	git pull
 	cd || exit
 done
+
+for d in ~/projects/*; do
+	echo "$d"
+	cd "$d"
+	git fetch -p --all
+	git pull
+	cd || exit
+done
+
+for d in ~/svnprojects/*; do
+	echo "$d"
+	cd "$d"
+	svn update
+	cd || exit
+done
