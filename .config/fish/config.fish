7368230764624b6c8d6721ead91c63243e84d684
Ismaël Mejía <iemejia@gmail.com>
2026-05-22 12:42:40 +0200

    [fish] Add nvm to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 # Go
 fish_add_path (go env GOPATH)/bin
 
+# NVM Node path
+set -gx NVM_DIR "$HOME/.nvm"
+fish_add_path "$NVM_DIR/versions/node/v26.2.0/bin"
+
 if status is-interactive
     # fzf key bindings and completion (Ctrl+R, Ctrl+T, Alt+C)
     fzf --fish | source
