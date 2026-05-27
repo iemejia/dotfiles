c8e2f18b9570a8b5f3b907dfd66875db22e5e3db
Ismaël Mejía <iemejia@gmail.com>
2026-05-27 15:24:28 +0200

    Use DOTFILES variable in install-minimal.sh and install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+DOTFILES=~/repositories/dotfiles
+
 mkdir -p ~/.local/bin
 
-ln -sfv ~/repositories/dotfiles/update-docker.sh ~/.local/bin/update-docker.sh
-ln -sfv ~/repositories/dotfiles/update-env.sh ~/.local/bin/update-env.sh
-ln -sfv ~/repositories/dotfiles/update-linux.sh ~/.local/bin/update-linux.sh
-ln -sfv ~/repositories/dotfiles/update-mac.sh ~/.local/bin/update-mac.sh
-ln -sfv ~/repositories/dotfiles/update-repositories.sh ~/.local/bin/update-repositories.sh
-ln -sfv ~/repositories/dotfiles/update-weekly.sh ~/.local/bin/update-weekly.sh
+ln -sfv "$DOTFILES/update-docker.sh" ~/.local/bin/update-docker.sh
+ln -sfv "$DOTFILES/update-env.sh" ~/.local/bin/update-env.sh
+ln -sfv "$DOTFILES/update-linux.sh" ~/.local/bin/update-linux.sh
+ln -sfv "$DOTFILES/update-mac.sh" ~/.local/bin/update-mac.sh
+ln -sfv "$DOTFILES/update-repositories.sh" ~/.local/bin/update-repositories.sh
+ln -sfv "$DOTFILES/update-weekly.sh" ~/.local/bin/update-weekly.sh
 
-ln -sfv ~/repositories/dotfiles/.config/opencode/opencode.json ~/.config/opencode/opencode.json
+ln -sfv "$DOTFILES/.config/opencode/opencode.json" ~/.config/opencode/opencode.json
 
--- install-minimal.sh
+++ install-minimal.sh
@@ -1,31 +1,33 @@
 #!/usr/bin/env bash
 
-ln -sfv ~/repositories/dotfiles/.bash_aliases ~/.bash_aliases
-ln -sfv ~/repositories/dotfiles/.bash_profile ~/.bash_profile
-ln -sfv ~/repositories/dotfiles/.bashrc ~/.bashrc
-ln -sfv ~/repositories/dotfiles/.profile ~/.profile
+DOTFILES=~/repositories/dotfiles
+
+ln -sfv "$DOTFILES/.bash_aliases" ~/.bash_aliases
+ln -sfv "$DOTFILES/.bash_profile" ~/.bash_profile
+ln -sfv "$DOTFILES/.bashrc" ~/.bashrc
+ln -sfv "$DOTFILES/.profile" ~/.profile
 
 mkdir -p ~/.config/fish/conf.d ~/.config/fish/functions
-ln -sfv ~/repositories/dotfiles/.config/fish/config.fish ~/.config/fish/
-ln -sfv ~/repositories/dotfiles/.config/fish/conf.d/env.fish ~/.config/fish/conf.d/
-ln -sfv ~/repositories/dotfiles/.config/fish/conf.d/aliases.fish ~/.config/fish/conf.d/
-for f in ~/repositories/dotfiles/.config/fish/functions/*.fish; do
+ln -sfv "$DOTFILES/.config/fish/config.fish" ~/.config/fish/
+ln -sfv "$DOTFILES/.config/fish/conf.d/env.fish" ~/.config/fish/conf.d/
+ln -sfv "$DOTFILES/.config/fish/conf.d/aliases.fish" ~/.config/fish/conf.d/
+for f in "$DOTFILES"/.config/fish/functions/*.fish; do
     ln -sfv "$f" ~/.config/fish/functions/
 done
 
-ln -sfv ~/repositories/dotfiles/.git_commit_msg.txt ~/.git_commit_msg.txt
-ln -sfv ~/repositories/dotfiles/.gitconfig ~/.gitconfig
-ln -sfv ~/repositories/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sfv ~/repositories/dotfiles/.vim ~/.vim
-ln -sfv ~/repositories/dotfiles/.vimrc ~/.vimrc
+ln -sfv "$DOTFILES/.git_commit_msg.txt" ~/.git_commit_msg.txt
+ln -sfv "$DOTFILES/.gitconfig" ~/.gitconfig
+ln -sfv "$DOTFILES/.tmux.conf" ~/.tmux.conf
+ln -sfv "$DOTFILES/.vim" ~/.vim
+ln -sfv "$DOTFILES/.vimrc" ~/.vimrc
 
 # subversion
 mkdir -p ~/.subversion
-ln -sfv ~/repositories/dotfiles/.subversion/servers ~/.subversion/servers
+ln -sfv "$DOTFILES/.subversion/servers" ~/.subversion/servers
 
 mkdir -p ~/.m2
-ln -sfv ~/repositories/dotfiles/.m2/settings.xml ~/.m2/settings.xml
+ln -sfv "$DOTFILES/.m2/settings.xml" ~/.m2/settings.xml
 
 mkdir -p ~/.gradle
-ln -sfv ~/repositories/dotfiles/.gradle/gradle.properties ~/.gradle/gradle.properties
+ln -sfv "$DOTFILES/.gradle/gradle.properties" ~/.gradle/gradle.properties
 
